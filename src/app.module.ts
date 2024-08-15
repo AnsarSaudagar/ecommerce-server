2f716b0cf45ad1aab4f2c3517108816b1c0d86a4
Ansar <champcena4037@gmail.com>
2024-08-15 17:57:15 +0530

    1) Installed typeorm 2) Mysql Typeorm connection done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,31 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { getConnectionManager } from 'typeorm';
+require('dotenv').config();
+
+const connectionManager = getConnectionManager();
+console.log(connectionManager);
+
 
 @Module({
-  imports: [],
+  imports: [
+    TypeOrmModule.forRoot(
+      {
+        type: 'mysql',
+        host: process.env.DATABASE_HOST,
+        port: +process.env.DATABASE_PORT,
+        username: process.env.DATABASE_USERNAME,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        entities: [
+            __dirname + '/../**/*.entity{.ts,.js}',
+        ],
+        synchronize: true,
+    }
+    )
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
